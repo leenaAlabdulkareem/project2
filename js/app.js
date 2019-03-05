b4edae9389c93bf5b74f004b228b9a7e3d150021
leena kareem <leena-101@hotmail.com>
2019-03-06 00:09:21 +0300

    leenaKr project2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,85 @@
 /*
   Please add all Javascript code to this file.
 */
+
+//I want to show my users live top and breaking news headlines
+$("#breakingNews").on('click', function(){
+var url1 = 'https://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=61078bf91b0b4f99b2bac9d769340140';
+var req = new Request(url1);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
+
+    $('#popup').on('click',function(){
+      $("#popUp").removeClass("hidden").removeClass("loader")
+      $("article").hide();
+        });
+  
+    $('.closePopUp').on('click',function(){
+      $('#popUp').addClass('hidden')       
+      $("article").show();
+    });
+
+            $("#popUp").append(pop)
+  });
+
+
+
+
+// If you want headlines just from a specific source, for example BBC News,
+$("#headlines").on('click', function(){
+ var url2= 'https://newsapi.org/v2/top-headlines?' +
+ 'sources=bbc-news&' +
+     'apiKey=61078bf91b0b4f99b2bac9d769340140';
+  var req = new Request(url2);
+    fetch(req)
+    .then(function(response) {
+     console.log(response.json());
+        })
+
+        $('#popup').on('click',function(){
+          $("#popUp").removeClass("hidden").removeClass("loader")
+          $("article").hide();
+            });
+      
+        $('.closePopUp').on('click',function(){
+          $('#popUp').addClass('hidden')       
+          $("article").show();
+        });
+    
+                $("#popUp").append(pop)
+      });
+
+
+
+
+    //I want to search for news articles that mention a specific topic or keyword
+    $("#searchfornews").on('click', function(){
+    var url3 = 'https://newsapi.org/v2/everything?' +'q=Apple&' +
+    'from=2019-03-05&' +'sortBy=popularity&' +
+    'apiKey=61078bf91b0b4f99b2bac9d769340140';
+
+    var req = new Request(url3);
+
+    fetch(req)
+    .then(function(response) {
+      console.log(response.json());
+    })
+$('#popup').on('click',function(){
+      $("#popUp").removeClass("hidden").removeClass("loader")
+      $("article").hide();
+        });
+  
+    $('.closePopUp').on('click',function(){
+      $('#popUp').addClass('hidden')       
+      $("article").show();
+    });
+
+            $("#popUp").append(pop)
+    
+  });
+
+   
